67a1a6ef0ac2017a37a222e98620a2543e5c6342
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2024-09-04 19:51:09 -0300

    TAbela

---
+++ Variavel de preço.py
@@ -0,0 +1,16 @@
+#f
+p1 = 100
+p2 = 200
+p3 = 300
+
+t = p1+p2+p3
+
+desc = 0
+if t > 500:
+    desc = t * 0.1
+    result = t - desc
+    r = t - desc
+    
+    print("Total da compra", t)
+    print("Desconto aplicado", desc)
+    print ("Total com desconto", r)
---
+++ teste.py
@@ -0,0 +1,6 @@
+def calcularmult(largura,altura):
+    return(a*b)
+a=20
+b=20
+resultado= a*b 
+print(resultado)
